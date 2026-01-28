907babac33e154abf478d8354b6ce482cb3820ff
Jon Bernard <jon@jonbernard.net>
2026-01-28 18:51:11 -0500

    initial migration

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,8 @@ export PATH="/app/tools/node_modules/.bin:$PATH"
 echo "Running database migrations..."
 prisma db push --skip-generate
 echo "Migrations complete."
+echo "Running initial import..."
+tsx scripts/import.ts
+echo "Import complete."
 export HOSTNAME=0.0.0.0
 exec "$@"
